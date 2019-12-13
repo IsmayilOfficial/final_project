d16fd15df09d68c2193b3db57f3bc62cfcd5e22e
IsmayilOfficial <tahmazov.ismayil@gmail.com>
2019-12-13 03:23:54 +0200

    version1

---
+++ myShop/app/src/main/java/ut/ee/cs/myshop/jsonEntityObjects/ServerObject.java
@@ -0,0 +1,16 @@
+package ut.ee.cs.myshop.jsonEntityObjects;
+
+public class ServerObject {
+
+    private String success;
+
+    public ServerObject(String success) {
+        this.success = success;
+    }
+
+    public String getSuccess() {
+        return success;
+    }
+}
+
+
